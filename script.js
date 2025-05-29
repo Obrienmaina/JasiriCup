f37a457912bf76b7e061b9908ef38069d08e48ea
Obrienmaina <89578689+Obrienmaina@users.noreply.github.com>
2025-05-29 15:30:24 +0200

    batch edit of home page

---
+++ script.js
@@ -0,0 +1,17 @@
+
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".banner-slide");
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle("active", i === index);
+    });
+  }
+
+  function changeSlide(direction) {
+    currentSlide = (currentSlide + direction + slides.length) % slides.length;
+    showSlide(currentSlide);
+  }
+
+setInterval(() => changeSlide(1), 8000);
+
